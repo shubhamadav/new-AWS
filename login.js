503266f91a4bc86aa934a7168ddfd3d6a0343409
shubhamadav <shubhamyad100@gmail.com>
2023-08-21 19:12:05 +0530

    hiding passward

--- login.js
+++ login.js
@@ -1,35 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('loginForm');
-    const signupButton = document.getElementById('signupButton');
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
 
-    loginForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        
-        const formData = new FormData(loginForm);
-        const userData = {
-            useremail: formData.get('useremail'),
-            userpass: formData.get('userpass')
-        };
+app.use(express.json())
 
-       
-        fetch('YOUR_API_URL', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData)
-        })
-        .then(response => response.json())
-        .then(data => {
-           
-            console.log(data); 
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-    });
+const users = []
 
-    signupButton.addEventListener('click', function () {
-        
-    });
-});
+app.get('/users',(req , res)=>{
+    res.json(users)
+})
+  
+app.post('/users',async (req , res)=>{
+    try{
+        const salt = await bcrypt.genSalt();
+        const hashpassword = await bcrypt.hash(req.body.password , salt)
+        console.log(salt);
+        console.log(hashpassword)
+        const user = {name:req.body.name , password:hashpassword}
+        users.push(user)
+       res.status(201).send
+    }
+    catch{
+            res.status(500).send();
+    }
+})
+
+app.listen(3000);
